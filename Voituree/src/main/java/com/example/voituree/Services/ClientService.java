607f1eb4bae99bb22b373b1f146d526f384717c4
Nssaiba <131682630+Nssaiba@users.noreply.github.com>
2025-12-03 00:33:38 +0100

    Update ClientService.java

--- Voituree/src/main/java/com/example/voituree/Services/ClientService.java
+++ Voituree/src/main/java/com/example/voituree/Services/ClientService.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name="SERVICE-CLIENT")
 public interface ClientService {
-    @GetMapping(path="/clients/{id}")
+    @GetMapping(path="/client/{id}")
     Client clientById(@PathVariable Long id);
 }
